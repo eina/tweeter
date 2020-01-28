b2220a48c203de21c09ca0b29360facbef797005
Eina Onting <einers@outlook.com>
2020-01-28 14:08:01 -0800

    create form submit handler for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,12 +32,25 @@ const data = [
 
 // svg definitions for button use, is this the best place to put this?
 const svgIcons = {
-  flag: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-flag"><path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path><line x1="4" y1="22" x2="4" y2="15"></line></svg>`,
-  retweet: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>`,
-  heart: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>`
+  flag:
+    '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-flag"><path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path><line x1="4" y1="22" x2="4" y2="15"></line></svg>',
+  retweet:
+    '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg>',
+  heart:
+    '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>'
 };
 
 $(document).ready(function() {
+  // handle form submission
+  $("#newTweetForm").submit(function(event) {
+    event.preventDefault();
+    const url = $(this).attr("action");
+    const query = $(this).serialize();
+    $.ajax(url, { method: "POST", data: query }).then(tweets => {
+      console.log("tweets?", tweets);
+      return tweets;
+    });
+  });
   /**
    * Create <article> element to show single tweet
    * @param {object} tweetData
@@ -67,7 +80,11 @@ $(document).ready(function() {
 
     return $tweet;
   };
-
+  /**
+   * Loops through an array of tweet objects
+   * to return an element to be appended
+   * @param {array} tweets an array of tweet objects
+   */
   const renderTweets = tweets => {
     tweets.forEach(tweetObj => {
       const $tweet = createTweetElement(tweetObj);
@@ -76,7 +93,4 @@ $(document).ready(function() {
   };
 
   renderTweets(data);
-
-  // const $tweet = createTweetElement(tweetData);
-  // console.log("tweet?", $tweet);
 });
